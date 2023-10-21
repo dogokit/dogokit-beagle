5a412dce58b72daf903494589783732c79d990d5
M Haidar Hanif <git@mhaidarhanif.com>
2023-10-21 10:58:28 +0700

    chore: various configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,37 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
-};
+	extends: [
+		'@remix-run/eslint-config',
+		'@remix-run/eslint-config/node',
+		'prettier',
+		'plugin:tailwindcss/recommended',
+	],
+	plugins: ['prettier'],
+	rules: {
+		'no-empty-pattern': 'off',
+		'@typescript-eslint/consistent-type-imports': [
+			'warn',
+			{
+				prefer: 'type-imports',
+				disallowTypeAnnotations: true,
+				fixStyle: 'inline-type-imports',
+			},
+		],
+		'import/no-duplicates': ['warn', { 'prefer-inline': true }],
+		'import/consistent-type-specifier-style': ['warn', 'prefer-inline'],
+		'import/order': [
+			'warn',
+			{
+				alphabetize: { order: 'asc', caseInsensitive: true },
+				groups: [
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+				],
+			},
+		],
+	},
+}
