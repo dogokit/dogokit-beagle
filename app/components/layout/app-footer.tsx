cfbeea95fbc4ccdd5bba70625940990e83350eac
M Haidar Hanif <git@mhaidarhanif.com>
2023-11-14 16:16:46 +0700

    feat: app footer status

--- app/components/layout/app-footer.tsx
+++ app/components/layout/app-footer.tsx
@@ -1,10 +1,16 @@
 import { ThemeMenu } from "~/components/shared/theme-menu"
+import { useRootLoaderData } from "~/hooks/use-root-loader-data"
 
 export function AppFooter() {
+  const { ENV } = useRootLoaderData()
+
   return (
     <footer className="flex items-center justify-between gap-2 border-t border-t-border px-2 py-1">
       <div className="text-muted-foreground">
-        <p className="text-xs">Status: OK</p>
+        <p className="text-xs">
+          <span>Status: </span>
+          <code>{ENV.NODE_ENV}</code>
+        </p>
       </div>
 
       <div>
