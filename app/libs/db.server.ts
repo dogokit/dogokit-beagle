42f34d652ec721ec0424feaaf3daa781c894fbf3
M Haidar Hanif <git@mhaidarhanif.com>
2023-11-15 06:45:49 +0700

    feat: improve prisma db.server

--- app/libs/db.server.ts
+++ app/libs/db.server.ts
@@ -1,17 +1,43 @@
 import { PrismaClient } from "@prisma/client"
+import chalk from "chalk"
 
 import { remember } from "~/utils/remember"
 
-let prisma = remember("prisma", () => new PrismaClient())
+export const prisma = remember("prisma", () => {
+  /**
+   * If there're some changes, need to restart the dev server
+   */
 
-declare global {
-  var __db__: PrismaClient | undefined
-}
+  const LOG_THRESHOLD = 20
 
-if (!global.__db__) {
-  global.__db__ = remember("prisma", () => new PrismaClient())
-}
-prisma = global.__db__
-prisma.$connect()
+  const client = new PrismaClient({
+    log: [
+      { level: "query", emit: "event" },
+      { level: "error", emit: "stdout" },
+      { level: "warn", emit: "stdout" },
+    ],
+  })
 
-export { prisma }
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  client.$on("query", event => {
+    if (event.duration < LOG_THRESHOLD) return
+
+    const color =
+      event.duration < LOG_THRESHOLD * 1.1
+        ? "green"
+        : event.duration < LOG_THRESHOLD * 1.2
+        ? "blue"
+        : event.duration < LOG_THRESHOLD * 1.3
+        ? "yellow"
+        : event.duration < LOG_THRESHOLD * 1.4
+        ? "redBright"
+        : "red"
+    const dur = chalk[color](`${event.duration}ms`)
+
+    console.info(`💎 Prisma | ${dur} | ${event.query}`)
+  })
+
+  client.$connect()
+
+  return client
+})
