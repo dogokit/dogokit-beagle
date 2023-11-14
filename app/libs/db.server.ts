f74599a5a64c8195c10c45f1727c551c4fb3d89f
M Haidar Hanif <git@mhaidarhanif.com>
2023-11-15 06:31:47 +0700

    fix: another issue with db.server

--- app/libs/db.server.ts
+++ app/libs/db.server.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client"
 
-import { parsedEnv } from "~/utils/env.server"
 import { remember } from "~/utils/remember"
 
 let prisma = remember("prisma", () => new PrismaClient())
@@ -9,14 +8,10 @@ declare global {
   var __db__: PrismaClient | undefined
 }
 
-if (parsedEnv.NODE_ENV === "production") {
-  prisma = remember("prisma", () => new PrismaClient())
-} else {
-  if (!global.__db__) {
-    global.__db__ = remember("prisma", () => new PrismaClient())
-  }
-  prisma = global.__db__
-  prisma.$connect()
+if (!global.__db__) {
+  global.__db__ = remember("prisma", () => new PrismaClient())
 }
+prisma = global.__db__
+prisma.$connect()
 
 export { prisma }
