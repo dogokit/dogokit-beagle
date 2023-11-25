a0e7ec9d73d3c50c2e6d295a91d0c124a8422e43
M Haidar Hanif <git@mhaidarhanif.com>
2023-11-26 02:06:35 +0700

    feat: better site footer

--- app/components/layout/site-footer.tsx
+++ app/components/layout/site-footer.tsx
@@ -4,8 +4,8 @@ import { getCurrentYear } from "~/utils/datetime"
 
 export function SiteFooter() {
   return (
-    <footer className="mt-20 bg-muted px-4 py-8">
-      <div className="flex items-center justify-between gap-2">
+    <footer className="mt-20 p-2 sm:p-4">
+      <div className="flex items-center justify-between gap-2 rounded-md bg-secondary px-4 py-8">
         <p>
           &copy; {getCurrentYear()} {configSite.name}. All rights reserved.
         </p>
