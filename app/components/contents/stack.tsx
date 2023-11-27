77587c60220ab46f7bcfa23d5692d90db6b85a1e
M Haidar Hanif <git@mhaidarhanif.com>
2023-11-27 19:22:11 +0700

    refactor: contents stack json data into tsx file

--- app/components/contents/stack.tsx
+++ app/components/contents/stack.tsx
@@ -6,9 +6,6 @@ import { Label } from "~/components/ui/label"
 import { Switch } from "~/components/ui/switch"
 import { cn } from "~/utils/cn"
 
-import serviceStackItems from "./service-stack-items.json"
-import techStackItems from "./tech-stack-items.json"
-
 export function ContentStack() {
   const [items, setItems] = useState<StackItem[]>(
     techStackItems.filter(item => item.isCore),
@@ -110,3 +107,206 @@ function StackItems({
     </ul>
   )
 }
+
+const techStackItems = [
+  {
+    name: "Remix",
+    icon: "simple-icons:remix",
+    url: "https://remix.run",
+    isCore: true,
+  },
+  {
+    name: "React",
+    icon: "logos:react",
+    url: "https://react.dev",
+    isCore: true,
+  },
+  {
+    name: "Vite",
+    icon: "logos:vitejs",
+    url: "https://vitejs.dev",
+    isSoon: true,
+  },
+  {
+    name: "Node.js",
+    icon: "logos:nodejs-icon",
+    url: "https://nodejs.org",
+  },
+  {
+    name: "TypeScript",
+    icon: "logos:typescript-icon",
+    url: "https://typescriptlang.org",
+    isCore: true,
+  },
+  {
+    name: "JavaScript",
+    icon: "logos:javascript",
+    url: "https://developer.mozilla.org/JavaScript",
+  },
+  {
+    name: "HTML",
+    icon: "logos:html-5",
+    url: "https://developer.mozilla.org/HTML",
+  },
+  {
+    name: "CSS",
+    icon: "logos:css-3",
+    url: "https://developer.mozilla.org/CSS",
+  },
+  {
+    name: "PostCSS",
+    icon: "logos:postcss",
+    url: "https://postcss.org",
+  },
+  {
+    name: "Autoprefixer",
+    icon: "logos:autoprefixer",
+    url: "https://autoprefixer.org",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: "logos:tailwindcss-icon",
+    url: "https://tailwindcss.com",
+    isCore: true,
+  },
+  {
+    name: "Radix UI",
+    icon: "tabler:brand-radix-ui",
+    url: "https://radix-ui.com",
+    isCore: true,
+  },
+  {
+    name: "shadcn UI",
+    icon: "tabler:square-rounded-letter-s",
+    url: "https://ui.shadcn.com",
+    isCore: true,
+  },
+  {
+    name: "Prisma ORM",
+    icon: "simple-icons:prisma",
+    url: "https://prisma.io",
+    isCore: true,
+  },
+  {
+    name: "MySQL",
+    icon: "logos:mysql-icon",
+    url: "https://mysql.org",
+    isCore: true,
+  },
+  {
+    name: "Docker",
+    icon: "logos:docker-icon",
+    url: "https://docker.com",
+    isCore: true,
+  },
+  {
+    name: "Conform",
+    icon: "tabler:square-rounded-letter-c",
+    url: "https://conform.guide",
+    isCore: true,
+  },
+  {
+    name: "Zod",
+    icon: "logos:zod",
+    url: "https://zod.dev",
+    isCore: true,
+  },
+  {
+    name: "React Email",
+    icon: "tabler:square-rounded-letter-r",
+    url: "https://react.email",
+    isCore: true,
+  },
+  {
+    name: "VS Code",
+    icon: "logos:visual-studio-code",
+    url: "https://code.visualstudio.com",
+  },
+  {
+    name: "Prettier",
+    icon: "logos:prettier",
+    url: "https://prettier.org",
+  },
+  {
+    name: "ESLint",
+    icon: "logos:eslint",
+    url: "https://eslint.org",
+  },
+  {
+    name: "MSW",
+    icon: "logos:msw-icon",
+    url: "https://mswjs.io",
+    isSoon: true,
+  },
+  {
+    name: "Vitest",
+    icon: "logos:vitest",
+    url: "https://vitest.dev",
+    isSoon: true,
+  },
+  {
+    name: "Testing Library",
+    icon: "logos:testing-library",
+    url: "https://testing-library.com",
+    isSoon: true,
+  },
+  {
+    name: "Playwright",
+    icon: "logos:playwright",
+    url: "https://playwright.io",
+    isSoon: true,
+  },
+]
+
+const serviceStackItems = [
+  {
+    name: "PlanetScale",
+    icon: "simple-icons:planetscale",
+    url: "https://planetscale.com",
+  },
+  {
+    name: "Vercel",
+    icon: "simple-icons:vercel",
+    url: "https://vercel.com",
+  },
+  {
+    name: "GitHub",
+    icon: "simple-icons:github",
+    url: "https://github.com",
+  },
+  {
+    name: "Uploadcare",
+    icon: "tabler:square-rounded-letter-u",
+    url: "https://uploadcare.com",
+  },
+  {
+    name: "Resend",
+    icon: "tabler:square-rounded-letter-r",
+    url: "https://resend.com",
+    isSoon: true,
+  },
+  {
+    name: "ConvertKit",
+    icon: "tabler:square-rounded-letter-c",
+    url: "https://convertkit.com",
+    isSoon: true,
+  },
+  {
+    name: "Sentry",
+    icon: "logos:sentry-icon",
+    url: "https://sentry.io",
+    isSoon: true,
+  },
+  {
+    name: "Novu",
+    icon: "tabler:square-rounded-letter-n",
+    url: "https://novu.co",
+    isSoon: true,
+  },
+  {
+    name: "OpenStatus",
+    icon: "tabler:square-rounded-letter-o",
+    url: "https://openstatus.dev",
+    isSoon: true,
+  },
+]
