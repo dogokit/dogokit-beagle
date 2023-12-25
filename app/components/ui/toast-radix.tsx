24dad7c4863d4ff8cfd4a1791542f23728473e7f
M Haidar Hanif <git@mhaidarhanif.com>
2023-12-25 22:04:11 +0700

    feat: toast sonner ui component
    toast radix rename

--- app/components/ui/toast-radix.tsx
+++ app/components/ui/toast-radix.tsx
@@ -3,7 +3,14 @@ import { cva, type VariantProps } from "class-variance-authority"
 import * as React from "react"
 
 import { Iconify } from "~/components/ui/iconify"
+import { useToast } from "~/components/ui/toast-radix-hook"
 import { cn } from "~/utils/cn"
+
+/**
+ * Toast with Radix UI
+ *
+ * More customizable than Toast Sonner
+ */
 
 const ToastProvider = ToastPrimitives.Provider
 
@@ -116,11 +123,36 @@ type ToastProps = React.ComponentPropsWithoutRef<typeof Toast>
 
 type ToastActionElement = React.ReactElement<typeof ToastAction>
 
+function Toaster() {
+  const { toasts } = useToast()
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  )
+}
+
 export {
   Toast,
   ToastAction,
   ToastClose,
   ToastDescription,
+  Toaster,
   ToastProvider,
   ToastTitle,
   ToastViewport,
---
+++ app/components/ui/toast-sonner.tsx
@@ -0,0 +1,37 @@
+import { useTheme } from "remix-themes"
+import { Toaster as Sonner, toast } from "sonner"
+
+/**
+ * Toast with Sonner
+ *
+ * Simpler than Toast Radix
+ */
+
+type ToasterProps = React.ComponentProps<typeof Sonner>
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const [theme] = useTheme()
+  const defaultTheme = theme ? theme : "light"
+
+  return (
+    <Sonner
+      theme={defaultTheme as ToasterProps["theme"]}
+      // eslint-disable-next-line tailwindcss/no-custom-classname
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
+            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton:
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
+      }}
+      {...props}
+    />
+  )
+}
+
+export { Toaster, toast }
