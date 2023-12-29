943dba36efa2f590ebac8682cb78693a75e4c6eb
M Haidar Hanif <git@mhaidarhanif.com>
2023-12-29 07:53:52 +0700

    docs: vite config build settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,23 @@ import { installGlobals } from "@remix-run/node"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
+/**
+ * Remix Plugin for Vite Config
+ *
+ * Remember to override the Build & Development Settings
+ *
+ * Build Command: `pnpm build`
+ * Output Directory: `build/client`
+ * Install Command: `pnpm install`
+ * Development Command: `pnpm dev`
+ *
+ * Because:
+ * The server is now compiled into build/server by default.
+ * The client is now compiled into build/client by default.
+ *
+ * https://remix.run/docs/en/main/future/vite#migrate-references-to-build-output-paths
+ */
+
 installGlobals()
 
 export default defineConfig({
