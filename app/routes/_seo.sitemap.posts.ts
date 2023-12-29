a5d3c75fa5c32d2848241fd9f2d83077c50345f2
M Haidar Hanif <git@mhaidarhanif.com>
2023-12-29 08:08:48 +0700

    fix: sitemap handle file cannot use model server file

--- app/routes/_seo.sitemap.posts.ts
+++ app/routes/_seo.sitemap.posts.ts
@@ -1,10 +1,17 @@
-import { modelPost } from "~/models/post.server"
+import { PrismaClient } from "@prisma/client"
+
 import { formatDateYMD } from "~/utils/datetime"
 import { type SEOHandle } from "~/utils/sitemap"
 
+const prisma = new PrismaClient()
+
 export const handle: SEOHandle = {
   getSitemapEntries: async () => {
-    const posts = await modelPost.getAllSlugs()
+    const posts = await prisma.post.findMany({
+      where: { status: { symbol: "PUBLISHED" } },
+      select: { slug: true, updatedAt: true },
+      orderBy: { slug: "asc" },
+    })
 
     return posts.map(post => {
       return {
--- app/routes/_seo.sitemap.users.ts
+++ app/routes/_seo.sitemap.users.ts
@@ -1,10 +1,16 @@
-import { modelUser } from "~/models/user.server"
+import { PrismaClient } from "@prisma/client"
+
 import { formatDateYMD } from "~/utils/datetime"
 import { type SEOHandle } from "~/utils/sitemap"
 
+const prisma = new PrismaClient()
+
 export const handle: SEOHandle = {
   getSitemapEntries: async () => {
-    const users = await modelUser.getAllUsernames()
+    const users = await prisma.user.findMany({
+      select: { username: true, updatedAt: true },
+      orderBy: { username: "asc" },
+    })
 
     return users.map(user => {
       return {
