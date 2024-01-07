c840a5e320d079fd6c6e4c8d00a1561b962655d1
M Haidar Hanif <git@mhaidarhanif.com>
2024-01-07 09:43:42 +0700

    feat: use remixicon to replace simple-icons

--- app/components/libs/icon-set.tsx
+++ app/components/libs/icon-set.tsx
@@ -4,10 +4,9 @@
  * Alternative of Iconify if want to preload icons, without fetching to their API
  *
  * - Phosphor Icons: https://phosphoricons.com
- * - Simple Icons: https://simpleicons.org
+ * - Remix Icon: https://remixicon.com
  */
 
-import { SiGithub, SiRemix } from "@icons-pack/react-simple-icons"
 import {
   House,
   Info,
@@ -18,17 +17,18 @@ import {
   Sun,
   type Icon,
 } from "@phosphor-icons/react"
+import { RiGithubFill, RiRemixRunFill } from "@remixicon/react"
 
 export type IconPhosphor = Icon
 
 export const IconSet = {
-  Laptop,
-  Moon,
-  Sun,
-  List,
-  Plus,
   House,
   Info,
-  Remix: SiRemix,
-  GitHub: SiGithub,
+  Laptop,
+  List,
+  Moon,
+  Plus,
+  Sun,
+  GitHub: RiGithubFill,
+  Remix: RiRemixRunFill,
 }
